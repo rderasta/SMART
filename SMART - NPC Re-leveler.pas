df528dc823971e1a26cab231e6442294dbe064d7
rderasta <63270260+rderasta@users.noreply.github.com>
2020-04-26 12:18:14 -0300

    Update SMART - NPC Re-leveler.pas

--- SMART - NPC Re-leveler.pas
+++ SMART - NPC Re-leveler.pas
@@ -1,7 +1,7 @@
 {
   Re-calculate NPC Lvl based on its race, size, max level and health.
   SMART - By Rasta
- }
+}
 unit userscript;
 
 uses mteFunctions;
@@ -10,8 +10,9 @@ function Process(e: IInterface): integer;
 
 var
 
- iRaceHP, iSize, iClassW, iHP, iHPOff, iNewHP, iLevel, iNewLevel, iShiftLvl, iSmash : integer;
- 	
+iSize, iStats, iHPOff, iMOff, iSOff, iHP, iM, iS, iRaceH, iRaceM, iRaceS, iRace, iClassW, iOldLevel, iLevel, iMinLevel, iMaxLevel, iLimitLvl, iLvlCheck, iNewLevel, iSmash : integer;
+
+ 
 begin
 
  Result := 0;
@@ -22,40 +23,55 @@ begin
 
 	if Signature(e) = 'NPC_' then
 	
+	
 	// Grabbing stuff to calculate
-	iLevel := GetElementNativeValues(e, 'ACBS\Calc max level');
+	iOldLevel := GetElementNativeValues(e, 'ACBS\Level');
+	iMinLevel := GetElementNativeValues(e, 'ACBS\Calc min level');
+	iMaxLevel := GetElementNativeValues(e, 'ACBS\Calc max level');
+	iLevel := (iLevel + iMinLevel + iMaxLevel);
+	iHPOff := GetElementNativeValues(e, 'ACBS\Health Offset');
+	iMOff := GetElementNativeValues(e, 'ACBS\Magicka Offset');
+	iSOff := GetElementNativeValues(e, 'ACBS\Stamina Offset');
 	iHP := GetElementNativeValues(e, 'DNAM\Health');
-	iHPOff := GetElementNativeValues(e, 'ACBS\Health Offset');	
-	iRaceHP := GetElementNativeValues(LinksTo(ElementBySignature(e, 'RNAM')), 'DATA - DATA\Starting Health');
+	iM := GetElementNativeValues(e, 'DNAM\Magicka');
+	iS := GetElementNativeValues(e, 'DNAM\Stamina');
+	iStats := (iHPOff + iMOff + iSOff + iHP + iM + iS);
 	iSize := GetElementNativeValues(LinksTo(ElementBySignature(e, 'RNAM')), 'DATA - DATA\Size');
+	iRaceH := GetElementNativeValues(LinksTo(ElementBySignature(e, 'RNAM')), 'DATA - DATA\Starting Health');
+	iRaceS := GetElementNativeValues(LinksTo(ElementBySignature(e, 'RNAM')), 'DATA - DATA\Starting Stamina');
+	iRaceM := GetElementNativeValues(LinksTo(ElementBySignature(e, 'RNAM')), 'DATA - DATA\Starting Magicka');
+	iRace := (iRaceH + iRaceS + iRaceM);
 	iClassW := GetElementNativeValues(LinksTo(ElementBySignature(e, 'CNAM')), 'DATA - DATA\Attribute Weights\Weight #0 (Health)\');
-	iShiftLvl := 10;
 	iSmash := 100;
+		
 	
 	// Uncomment to remove Mult flags
-	// SetElementNativeValues(e, 'ACBS\Flags\PC Level Mult', '0');
-	// Comment to not skip them, must uncomment above.
-	if GetElementNativeValues(e, 'ACBS\Flags\PC Level Mult') then exit;
+	 SetElementNativeValues(e, 'ACBS\Flags\PC Level Mult', '0');
+	// Comment to NOT skip them, must uncomment above.
+	// if GetElementNativeValues(e, 'ACBS\Flags\PC Level Mult') then exit;
 	
+		
 	// Super scientificly accurate calculation of the stuff, and setting npc's level accordingly
 		if (iSize = '0') then
-			iNewLevel := (((iLevel * iShiftLvl) + ((iHP + iHPOff + iRaceHP) * iClassW)) / iSmash);
+			iNewLevel := (((iLevel * iClassW) + iRace + iStats) / iSmash);
 				SetElementNativeValues(e, 'ACBS\Level',(iNewLevel));
-				
+						
 		if (iSize = '1') then
-			iNewLevel := (((iLevel * iShiftLvl) + ((iHP + iHPOff + iRaceHP) * (iClassW + iSize))) / iSmash);
+			iNewLevel := (((iLevel * iClassW) + iRace + iRace + iStats) / iSmash);
 				SetElementNativeValues(e, 'ACBS\Level',(iNewLevel));
-				
-		if (iSize = '2') then
-			iNewLevel := (((iLevel * iShiftLvl) + ((iHP + iHPOff + iRaceHP) * iClassW) * iSize) / iSmash);
-				SetElementNativeValues(e, 'ACBS\Level',(iNewLevel));	  
+							
+		if (iSize > '1') then
+			iNewLevel := (((iLevel * iClassW) + (iRace * iSize) + iStats) / iSmash);
+				SetElementNativeValues(e, 'ACBS\Level',(iNewLevel));
+							
 		
-		if (iSize = '3') then
-			iNewLevel := (((iLevel * iShiftLvl) + ((iHP + iHPOff + iRaceHP) * iClassW) * iSize) / iSmash);
-				SetElementNativeValues(e, 'ACBS\Level',(iNewLevel));	
+	// Fix for NPCs with 55k h/m/s
+	iLimitLvl := 500;
+	iLvlCheck := GetElementNativeValues(e, 'ACBS\Level');
+		if (iLvlCheck > iLimitLvl) then
+			SetElementNativeValues(e, 'ACBS\Level',(iLimitLvl));
+			
 	
-	
-
 	if Signature(e) = 'NPC_' then exit;
 
 	
